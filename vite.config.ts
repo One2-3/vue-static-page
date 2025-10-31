4911c9ecbd41c9844b2ba81d326e821d09fdcdad
One2-3 <qkrwldb1695@naver.com>
2025-10-31 16:03:02 +0900

    init 오류 수정

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/<vue-static-workflows>/', // 예: '/vue-static-page/'
 })
+
